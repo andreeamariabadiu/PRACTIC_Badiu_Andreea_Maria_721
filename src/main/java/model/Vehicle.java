0c99e23eda0e023d99a38532b62945f90cf5cb70
Andreea Badiu <andreea.badiu@stud.ubbcluj.ro>
2026-01-14 08:43:52 +0200

    Vehicle Model

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,4 +1,75 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Vehicle {
+    private int id;
+    private String licensePlate;
+
+
+    @JsonProperty("type")
+    private VehicleType type;
+
+
+    private String ownerCity;
+
+    @JsonProperty("status")
+    private VehicleStatus status;
+
+
+    public Vehicle() {};
+
+    public Vehicle(int id, String licensePlate, VehicleType type, String ownerCity, VehicleStatus status) {
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.type = type;
+        this.ownerCity = ownerCity;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getOwnerCity() {
+        return ownerCity;
+    }
+
+    public void setOwnerCity(String ownerCity) {
+        this.ownerCity = ownerCity;
+    }
+
+    public VehicleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VehicleStatus status) {
+        this.status = status;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + licensePlate + " | " + type + " | " + status + " | " + "city=" + ownerCity;
+    }
 }
