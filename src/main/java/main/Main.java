b5acd3679d21155e5fd100041392678e16f71765
Andreea Badiu <andreea.badiu@stud.ubbcluj.ro>
2026-01-14 09:45:24 +0200

    Repo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,40 @@
 package main;
 
+import repository.SCTCSystemRepo;
+import service.SCTCSystemService;
+import model.*;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        System.out.println("=== Start ===");
+
+        // 1. Instanțiem Repository
+        SCTCSystemRepo repo = new SCTCSystemRepo();
+
+        // 2. Citim datele din fișiere
+        List<Vehicle> vehicles = repo.readVehicle();
+        List<Fine> fines = repo.readFines();
+        List<TrafficEvent> events = repo.readTrafficEvent();
+
+
+
+        // Afișăm toți șoferii (Cerința 1)
+        System.out.println("\n--- All Vehicles ---");
+        System.out.println(vehicles);
+        }
+
+
+
+//        // 3. Instanțiem Service-ul
+//        SCTCSystemRepo service = new SCTCSystemRepo(vehicles, fines, events);
+//
+//        // --- CERINȚA 2: Filtrare ---
+//        System.out.println("\n--- Filter by Type and Status ---");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Input type and status (ex: BUS ACTIVE): ");
+//        String teamName = scanner.nextLine();
+//
+//    }
+};
--- src/main/java/service/SCTCSystemService.java
+++ src/main/java/service/SCTCSystemService.java
@@ -1,4 +1,47 @@
 package service;
 
+import model.*; // Importăm modelele
+import java.io.PrintWriter; // Pentru scriere în fișier
+import java.util.*; // Pentru List, Map, Comparator
+import java.util.stream.Collectors; // Pentru Streams
+
 public class SCTCSystemService {
-}
+    // Listele de date cu care lucrăm
+    private List<Vehicle> vehicles;
+    private List<Fine> fines;
+    private List<TrafficEvent> trafficEvents;
+
+    public SCTCSystemService() {};
+
+    // Constructorul primește datele gata citite din Main
+    public SCTCSystemService(List<Vehicle> v, List<Fine> f, List<TrafficEvent> t) {
+        this.vehicles = v;
+        this.fines = f;
+        this.trafficEvents = t;
+    }
+
+    ;
+
+
+    // --- CERINȚA 2:
+
+    public List<Vehicle> filterByStatusAndType(String type, String status) {
+        // Deschidem stream-ul
+        return vehicles.stream()
+                .filter(v -> v.getType().equals(type))
+
+                .filter(v -> v.getStatus().equals(status))
+
+                .collect(Collectors.toList());
+    }
+
+//    //public List<Vehicle> sortVehicles() {
+//     //   return vehicles.stream()
+//      //          // Sortăm invers (.reversed) după SkillLevel
+//                .sorted(Comparator.comparing(VehicleType::getSkillLevel).reversed()
+//                        // Dacă skill-ul e egal, sortăm alfabetic după nume
+//                        .thenComparing(Fahrer::getName))
+//                .collect(Collectors.toList());
+//    }//
+};
+
