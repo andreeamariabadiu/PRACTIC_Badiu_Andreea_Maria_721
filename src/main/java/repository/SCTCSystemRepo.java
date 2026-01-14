480df505acbb1ba129f021290992809a5a5d730f
Andreea Badiu <andreea.badiu@stud.ubbcluj.ro>
2026-01-14 09:15:22 +0200

    Repo

--- src/main/java/repository/SCTCSystemRepo.java
+++ src/main/java/repository/SCTCSystemRepo.java
@@ -1,4 +1,44 @@
 package repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import model.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
 public class SCTCSystemRepo {
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+
+    public List<Vehicle> readVehicle() {
+        try {
+            return mapper.readValue(new File("vehicles.json"), new TypeReference<List<Vehicle>>(){});
+        } catch (IOException e) {
+            System.err.println("Error reading vehicles:  " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+
+    public List<Fine> readFines() {
+        try {
+            return mapper.readValue(new File("fines.json"), new TypeReference<List<Fine>>(){});
+        } catch (IOException e) {
+            System.err.println("Error reading fines:  " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+
+    public List<TrafficEvent> readTrafficEvent() {
+        try {
+            return mapper.readValue(new File("events.json"), new TypeReference<List<TrafficEvent>>(){});
+        } catch (IOException e) {
+            System.err.println("Error reading events:  " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
 }
